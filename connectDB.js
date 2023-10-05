63f3a3a5c5a7164e2fbb05d8def4b796a3efbdbd
Alexander Meleshko <121691028+AlexMel83@users.noreply.github.com>
2023-10-05 13:32:44 +0300

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -1,7 +1,6 @@
 const mongoos = require('mongoose');
 require('dotenv').config();
 
-const { DB_AC } = process.env;
-const { DB_PWD } = process.env;
+const { DB_AC, DB_PWD } = process.env;
 
 module.exports = mongoos.createConnection(`mongodb+srv://${DB_AC}:${DB_PWD}@cluster0.vgwsacm.mongodb.net/Train?retryWrites=true&w=majority`)
